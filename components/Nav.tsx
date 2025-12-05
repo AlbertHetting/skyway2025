f71d4a0fcda31002efc60c42c3e9fe528cbef7f7
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-12-05 11:44:06 +0100

    Adding nav in layout and adding icons

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,10 +3,33 @@ import Image from "next/image";
 
 export default function Nav() {
   return (
-    <nav>
-      <Link href={"/app/splash"}>
-        <Image></Image>
-        <h1>Nav test</h1>
+    <nav className="flex flex-row justify-evenly gap-10 mb-2 fixed ">
+      <Link href={"/app/widgets"}>
+        <Image
+          src="/UiIcons/WidgetAdd.png"
+          alt="Skyway logo"
+          width={400}
+          height={100}
+          className="size-20 drop-shadow-lg"
+        />
+      </Link>
+      <Link href={"/app/widgets"}>
+        <Image
+          src="/UiIcons/WidgetAdd.png"
+          alt="Skyway logo"
+          width={400}
+          height={100}
+          className="size-20 drop-shadow-lg"
+        />
+      </Link>
+      <Link href={"/app/widgets"}>
+        <Image
+          src="/UiIcons/WidgetAdd.png"
+          alt="Skyway logo"
+          width={400}
+          height={100}
+          className="size-20 drop-shadow-lg"
+        />
       </Link>
     </nav>
   );
