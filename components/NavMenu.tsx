0c56d303b9d74fcf11b23b4baab18d40c0b636f8
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-12-08 10:45:38 +0100

    fixing the return to prev. function

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavMenu() {
   const router = useRouter();
@@ -19,16 +19,14 @@ export default function NavMenu() {
         />
       </Link>
 
-      <Link href="/.">
-        <Image
-          src="/img/exit-icon-black.svg"
-          alt="Widget"
-          width={50}
-          height={50}
-          className="drop-shadow-lg"
-          onClick={() => router.back}
-        />
-      </Link>
+      <Image
+        src="/img/exit-icon-black.svg"
+        alt="Exit"
+        width={50}
+        height={50}
+        className="drop-shadow-lg"
+        onClick={() => router.back()}
+      />
     </nav>
   );
 }
